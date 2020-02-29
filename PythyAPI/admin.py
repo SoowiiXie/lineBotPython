dc930fc01b1d487fc51a31211939d92a41e3a0d8
soowii <e64009117@gs.ncku.edu.tw>
2020-02-29 18:27:58 +0800

    改變admin.py嘗試進入將狗admin

--- PythyAPI/admin.py
+++ PythyAPI/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from PythyAPI.models import users
+
+class usersAdmin(admin.ModelAdmin):
+    list_display=('uid','question')
+admin.site.register(users, usersAdmin)
