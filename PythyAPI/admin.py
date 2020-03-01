733e4649a93cc707fe7e0ca5ec93b1af105d005a
soowii <e64009117@gs.ncku.edu.tw>
2020-03-02 02:53:06 +0800

    雖然admin在heroku下沒有用 但還是先建好

--- PythyAPI/admin.py
+++ PythyAPI/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from PythyAPI.models import users
+from PythyAPI.models import users,teamUp
 
 class usersAdmin(admin.ModelAdmin):
-    list_display=('uid','question','state')
+    list_display=('uid','question','state','created_time')
 admin.site.register(users, usersAdmin)
+
+class teamUpAdmin(admin.ModelAdmin):
+    list_display=('bid','place','amount','timein')
+admin.site.register(teamUp, teamUpAdmin)
