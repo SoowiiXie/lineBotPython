17b50e2a8dbae5d48e72eb977cc90d65e83ab321
soowii <e64009117@gs.ncku.edu.tw>
2020-03-08 17:19:08 +0800

    pip完和加requirements後再試試

--- AngelaPythy/settings.py
+++ AngelaPythy/settings.py
@@ -15,9 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-#import django_heroku
+import django_heroku
 # Activate Django-Heroku.
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
