af2625d66570af7675d31dbd22be725dd4275cb2
soowii <e64009117@gs.ncku.edu.tw>
2020-03-29 20:39:50 +0800

    Revert "試著上傳首頁"
    This reverts commit 18292db9f6e88a8565cc4caa09e7925da7699e84.

--- AngelaPythy/wsgi.py
+++ AngelaPythy/wsgi.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AngelaPythy.settings')
 
