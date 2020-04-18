d673d7b234d448ccf387e99b1c4ed57bdb9f0c33
soowii <e64009117@gs.ncku.edu.tw>
2020-04-18 11:48:17 +0800

    上傳ODBC用PostgreSQL

--- module/func12liff.py
+++ module/func12liff.py
@@ -6,14 +6,52 @@ ConfirmTemplate,PostbackTemplateAction
 
 from PythyAPI.models import teamUp, users
 
-#import peewee #20200418
-#db = peewee.PostgresqlDatabase('daqfqhdshludoq',
-#                          user='tlnlkxrtnbepdl',
-#                          password='2a372ee7bedb7e93309cb56336a42fe8824885adb6a6509d27d86cdba914c5d3',
-#                          host='ec2-52-86-73-86.compute-1.amazonaws.com',
-#                          port=5432)
-#
-#db.connect()
+import peewee #20200418
+from datetime import date #20200418
+
+db = peewee.PostgresqlDatabase('daqfqhdshludoq',
+                          user='tlnlkxrtnbepdl',
+                          password='2a372ee7bedb7e93309cb56336a42fe8824885adb6a6509d27d86cdba914c5d3',
+                          host='ec2-52-86-73-86.compute-1.amazonaws.com',
+                          port=5432)
+
+db.connect() #20200418
+
+print("VO---------------------------------#20200418")
+#table
+class GROUPER(peewee.Model):
+    #col
+    GRP_NO = peewee.CharField()
+    MB_ID = peewee.CharField()
+    LOC_NO = peewee.CharField()
+    GRP_APPLYSTART = peewee.DateField()
+    GRP_APPLYEND = peewee.DateField()
+    GRP_START = peewee.DateField()
+    GRP_END = peewee.DateField()
+    GRP_NAME = peewee.CharField()
+    GRP_CONTENT = peewee.CharField()
+    GRP_PERSONMAX = peewee.IntegerField
+    GRP_PERSONMIN = peewee.IntegerField
+    GRP_PERSONCOUNT = peewee.IntegerField
+    GRP_STATUS = peewee.IntegerField
+    GRP_FOLLOW = peewee.IntegerField
+    
+    #db
+    class Meta:
+        database = db
+
+#table
+class GRP_DETAIL(peewee.Model):
+    #col
+    participants = peewee.ForeignKeyField(GROUPER, backref='participatingGroups')
+    GRP_NO = peewee.CharField()
+    MB_ID = peewee.CharField()
+    GRP_REGISTER = peewee.IntegerField
+
+    #db
+    class Meta:
+        database = db
+
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 
@@ -67,6 +105,11 @@ def manageForm(event, mtext, user_id):  #處理LIFF傳回的FORM資料
             timein = flist[2]
             unit = teamUp.objects.create(bid=user_id, place=place, amount=amount,\
                                           timein=timein)  #寫入資料庫
+            
+            print(timein)
+            participant = GROUPER.create(name=user_id, LOC_NO=place,\
+                                         GRP_PERSONMAX=amount,GRP_START=date(timein))
+            
             unit.save()
             text1 = "您的揪團資料如下："
             text1 += "\n場地：" + place
--- module/funcDB.py
+++ module/funcDB.py
@@ -8,95 +8,93 @@ db = peewee.PostgresqlDatabase('daqfqhdshludoq',
 
 db.connect()
 
-##table
-#class Person(peewee.Model):
-#    #col
-#    name = peewee.CharField()
-#    birthday = peewee.DateField()
-#    
-#    #db
-#    class Meta:
-#        database = db
-#
-##table
-#class Pet(peewee.Model):
-#    #col
-#    owner = peewee.ForeignKeyField(Person, backref='pets')
-#    name = peewee.CharField()
-#    animal_type = peewee.CharField()
-#    
-#    #db
-#    class Meta:
-#        database = db
+print("VO------------------------------------------")
+#table
+class Person(peewee.Model):
+    #col
+    name = peewee.CharField()
+    birthday = peewee.DateField()
+    
+    #db
+    class Meta:
+        database = db
+
+#table
+class Pet(peewee.Model):
+    #col
+    owner = peewee.ForeignKeyField(Person, backref='pets')
+    name = peewee.CharField()
+    animal_type = peewee.CharField()
+    
+    #db
+    class Meta:
+        database = db
 
 ##creat_table()
 #db.create_tables([Person, Pet])
 #
-##insert-1
+print("DAO-----------------------------------------")
+print("insert-1------------------------------------")
 from datetime import date
 #
 #uncle_bob = Person(name='Bob', birthday=date(1960, 1, 15))
 #uncle_bob.save() #return:1
 #
-##insert-2
+print()
+print("insert-2------------------------------------")
 grandma = Person.create(name='Grandma', birthday=date(1935, 3, 1))
 #herb = Person.create(name='Herb', birthday=date(1950, 5, 5))
-personDel = Person.create(name='personDel', birthday=date(1950, 5, 5))
+peopleDel = Person.create(name='PeopleDel', birthday=date(1950, 5, 5))
+peopleDel2 = Person.create(name='PeopleDel', birthday=date(1957, 7, 7))
 ##insert-pet
 #bob_kitty = Pet.create(owner=uncle_bob, name='Kitty', animal_type='cat')
 #herb_fido = Pet.create(owner=herb, name='Fido', animal_type='dog')
 #herb_mittens = Pet.create(owner=herb, name='Mittens', animal_type='cat')
 #herb_mittens_jr = Pet.create(owner=herb, name='Mittens Jr', animal_type='cat')
 
-#update
 print()
-print("update--------------------------------------")
+print("#update-------------------------------------")
 grandmaUpdate = Person.select().where(Person.name == 'Grandma').get()
 print(type(grandmaUpdate))
 grandmaUpdate.name = 'Grandma L.'
-grandmaUpdate.save() #returns:1
+print(grandmaUpdate.save(),'#returns:1')
 
-#get one by key
 print()
-print("get one by key------------------------------")
+print("#get one by key-----------------------------")
 herb = Person.select().where(Person.name == 'Herb').get()
 print(herb.name, herb.birthday)
-#get one by value
+
 print()
-print("--------------------------------------------")
+print("#get one by value---------------------------")
 grandma = Person.get(Person.name == 'Grandma L.')
 print(grandma.name, grandma.birthday)
 
-#get all
 print()
-print("--------------------------------------------")
-print(type(Person.select())) #不是真的python的list
+print("#get all------------------------------------")
+print(type(Person.select()),'#不是真的python的list') 
 
-#delete
 print()
-print("delete--------------------------------------")
-person = Person.get(Person.name=='personDel')
-print(person.delete_instance()) #return number of rows deleted
+print("#delete-------------------------------------")
+person = Person.get(Person.name=='Grandma L.')
+print(person.delete_instance(),'#return number of rows deleted')
 
-#delete all
 print()
-query = Person.delete().where(Person.name=='Grandma L.')
-query.execute() #return number of rows deleted
+print("#delete all---------------------------------")
+query = Person.delete().where(Person.name=='PeopleDel')
+print(query.execute(),'#return number of rows deleted')
 
-#使用select()，選取全部資料，再用迴圈一一列出
 print()
-print("--------------------------------------------")
+print("#使用select()，選取全部資料，再用迴圈列出------")
 for person in Person.select():
     print(person.id, person.name, person.birthday)
 
-#使用更多SQL語句    
 print()
-print("--------------------------------------------")
+print("#使用更多SQL語句 ----------------------------")
 query = Pet.select().where(Pet.animal_type == 'cat')
-
 for pet in query:
     print("Pet's name: ", pet.name, ": ", "Owner's name: ", pet.owner.name)
-    
-#關閉連線
+
+print()
+print("#關閉連線------------------------------------")
 if db:
     db.close()
